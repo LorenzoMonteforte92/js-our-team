1ea5674fb6baddd6e1e5852fc9cb3628b012fbfa
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-20 13:49:24 +0100

    feat: add objects array

--- JS/script.js
+++ JS/script.js
@@ -11,6 +11,40 @@
 // BONUS 2:
 // Organizzare i singoli membri in card/schede
 
+//creo array di oggetti con info membri del team
+let teamMembers =[
+    {
+        name: `Wayne Barnett`,
+        role: `Founder & Ceo`, 
+        img: `wayne-barnett-founder-ceo.jpg`
+    },
+    {
+        name: `Angela Caroll`,
+        role: `Chief Editor`, 
+        img: `angela-caroll-chief-editor.jpg`
+    },
+    {
+        name: `Walter Gordon`,
+        role: `Office Manager`, 
+        img: `walter-gordon-office-manager.jpg`
+    },
+    {
+        name: `Angela Lopez`,
+        role: `Social Media Manager`, 
+        img: `angela-lopez-social-media-manager.jpg`
+    },
+    {
+        name: `Scott Estrada`,
+        role: `Developer`, 
+        img: `scott-estrada-developer.jpg`
+    },
+    {
+        name: `Barbara Ramos`,
+        role: `Graphic Designer`, 
+        img: `barbara-ramos-graphic-designer.jpg`
+    }
+    
+]
 
 
 

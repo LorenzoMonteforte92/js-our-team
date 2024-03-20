2ae599b81db12d4400daf89a7b8c7708f9011164
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-20 14:12:38 +0100

    feat: add print members info in DOM

--- JS/script.js
+++ JS/script.js
@@ -46,6 +46,32 @@ let teamMembers =[
     
 ]
 
+let membersContainer = document.querySelector(`#name-container`);
+
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//creo un ciclo for che scorre l'array
+for (let i = 0; i < teamMembers.length; i++){
+    //salvare ogni indice in una variabile
+    let teamMembersIndex = teamMembers[i];
+    console.log("Nome: " + teamMembersIndex.name);
+    console.log("Ruolo: " + teamMembersIndex.role);
+    console.log("img url: " + teamMembersIndex.img);
+
+}
+
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+//creare un ciclo for che stampa in pagina le info dei membri
+for(let i = 0; i < teamMembers.length; i++){
+    let teamMembersIndex = teamMembers[i];
+    //aggiungere a membersContainer il mio snippett di codice con i dati dei membri
+    membersContainer.innerHTML += `
+        <div>
+            <h4>${teamMembersIndex[`name`]}</h4>
+            <p>${teamMembersIndex[`role`]}</p>
+            <p>${teamMembersIndex[`img`]}</p>
+        </div>
+    `
+}
 
 
 
